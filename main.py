01e118124e20b3d3b1c43148552ceab0f9f65078
Nikita Zhuravets <impozernovus@gmail.com>
2023-09-23 22:22:53 +0300

    test

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ db_path = 'app_data\\db.db'
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-print('12312asadaaa6')
+print('12312asasdadaaa6')
 
 
 class AssetStates(StatesGroup):
